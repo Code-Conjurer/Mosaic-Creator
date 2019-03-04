71c1b8235ac83089a294fd8e2574977555a0360b
Matthew Garneau <sadzune@gmail.com>
2019-03-03 16:55:16 -0800

    -Colors are now based off doubles instead of ints -Image array is now 2d (fixing previous bug)

--- src/Mosaic.java
+++ src/Mosaic.java
@@ -1,17 +1,12 @@
 
 import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.PixelReader;
-import javafx.scene.image.WritableImage;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -19,10 +14,14 @@ public class Mosaic extends Application {
 
 	public static File[] dirList;
 	public static Piece[] pieces;
-	public static final int sizeX = 80;
-	public static final int sizeY = 60;
+	public static final int sizeX = 8; //80
+	public static final int sizeY = 6; //60
 	private static final double opacity = 0;
 
+	int sceneHeight = 800;
+	int sceneWidth = 600;
+	int tileSizeX = 4;
+	int tileSizeY = 4;
 
 	public static void main(String[] args){
 
@@ -34,72 +33,63 @@ public class Mosaic extends Application {
 
 			pieces = new Piece[dirList.length];
 
-			int count = 0;
 			for(int i = 0; i < dirList.length; i++){
 				Image temp = new Image(dirList[i].toURI().toString());
 				pieces[i] = new Piece(temp);
-				count++;
-
 			}
+
 			mergeSort(pieces);
 
 		} catch (Exception e){
 			System.out.println(e);
 		}
-
-		/*for(Piece p : pieces){
-			System.out.println(p.getBrightness());
-		}*/
-
 		launch(args);
 	}
 
 	public void start(Stage theStage){
-		//StackPane pane = new StackPane();
-
 		Image img = null;
-
 		try {
-			img = new Image(getClass().getResource("/res/image/Color-Wheel-Basics-Full-CMY-color-wheel.jpg").toExternalForm());
+			img = new Image(getClass().getResource("/res/image/Trees.jpg").toExternalForm());
 
 
 		} catch (Exception e) {
 			System.out.println("file not found");
 		}
-		Image[] imageList = new Image[(int) ((img.getWidth()/sizeX)*(img.getHeight()/sizeY))];
-
+		//Image[] imageList = new Image[(int) ((img.getWidth()/sizeX)*(img.getHeight()/sizeY))]; //TODO: change this so there's no possible rounding error
+		Image[][] imageList = new Image[(int)(img.getWidth()/sizeX)][(int)(img.getHeight()/sizeY)];
 		double red = 0; double green = 0; double blue = 0;
-		int brightness;
+		double brightness;
 		int index = 0;
 
-		int counter = 0;
+		int pixelsCounted = 0;
 		Color temp = null;
 
-		for(int i = 0; i < img.getWidth(); i++){
-			for(int j = 0; j < img.getHeight(); j++){
+		for(int x = 0; x < img.getWidth(); x++){
+			for(int y = 0; y < img.getHeight(); y++){
 				//every 80x60 
-				if(i % sizeX == 0 && j % sizeY == 0 && (i != 0 && j != 0)){
-					red = red / (counter);
-					green = green / (counter);
-					blue = blue / (counter);
+				if(x % sizeX == 0 && y % sizeY == 0 && (x != 0 && y != 0)){
+					red = red / (pixelsCounted);
+					green = green / (pixelsCounted);
+					blue = blue / (pixelsCounted);
 
 					temp = new Color(red, green, blue, opacity);
-					brightness = toWebColor(temp.getBrightness());					
+					brightness = (temp.getBrightness());
 
-					imageList[index] = pieces[search(pieces, temp, brightness)].getImage();
-					
+					//imageList[index] = pieces[search(pieces, temp, brightness)].getImage();
+					imageList[(x/sizeX) - 1][(y/sizeY) - 1] = pieces[search(pieces, temp, brightness)].getImage();
+
 					index++;
 
 					//reset
 					red = 0;
 					green = 0;
 					blue = 0;
-					counter = 0;
+					pixelsCounted = 0;
 
 				}
 				PixelReader pr = img.getPixelReader();
-				temp = pr.getColor(i,j);
-				counter++;
+				temp = pr.getColor(x,y);
+				pixelsCounted++;
 				red += temp.getRed();
 				green += temp.getGreen();
 				blue += temp.getBlue();				
@@ -107,17 +97,17 @@ public class Mosaic extends Application {
 		}
 		
 		GridPane gp = new GridPane();
-		Scene scene = new Scene(gp, 1200, 1200);
+		Scene scene = new Scene(gp, sceneWidth, sceneHeight);
 
-		Image tempImage = null;
-		
-		for(int i = 0; i < img.getWidth()/sizeX ; i++ ){
-			for(int j = 0; j < img.getHeight()/sizeY ; j++){
+		for(int x = 0; x < img.getWidth()/sizeX ; x++ ){
+			for(int y = 0; y < img.getHeight()/sizeY ; y++){
 
-				tempImage = imageList[(int) (i * (img.getHeight()/sizeY) + j)];
+				//tempImage = imageList[(int) (x * (img.getHeight()/sizeY) + y)];
 				ImageView tile = new ImageView();
-				tile.setImage(tempImage);
-				gp.add(tile, i, j);
+				tile.setImage(imageList[x][y]);
+				tile.setFitHeight(tileSizeY);
+				tile.setFitWidth(tileSizeX);
+				gp.add(tile, x, y);
 			}
 		}
 
@@ -130,16 +120,16 @@ public class Mosaic extends Application {
 	 * compares the brightness of two colors within the bounds, then
 	 * uses ColorCheck if similar brightness
 	 */
-	public static int search(Piece[] pieces, Color color, int brightness){
+	public static int search(Piece[] pieces, Color color, double brightness){
 		int low = 0;
 		int high = pieces.length - 1;
-		final int BOUNDS = 10;
+		double MARGIN_OF_ERROR = 10.0/256.0; //Currently chosen arbitrarily
 
 		while( high >= low){
 			int mid = (low + high) / 2;
 
-			if(Math.abs(pieces[mid].getBrightness() - brightness) <= BOUNDS){
-				return colorCheck(pieces, mid, color, BOUNDS);
+			if(Math.abs(pieces[mid].getBrightness() - brightness) <= MARGIN_OF_ERROR){
+				return colorCheck(pieces, mid, color, MARGIN_OF_ERROR);
 			}
 			if(pieces[mid].getBrightness() < brightness){
 				low = mid + 1;
@@ -153,52 +143,50 @@ public class Mosaic extends Application {
 
 	}
 	/*
-	 * compares the int color values and returns if there in the bounds
-	 * recurses if the color is not found in the, by modifing the bounds
+	 * compares the color values and returns if there in the marginOfError
+	 * recurse if the color is not found in the, by modifying the marginOfError
 	 */
-	public static int colorCheck(Piece[] pieces,int index, Color c, int bounds){
+	public static int colorCheck(Piece[] pieces,int index, Color c, double marginOfError){
 
-		int red = toWebColor(c.getRed());
-		int green = toWebColor(c.getGreen());
-		int blue = toWebColor(c.getBlue());
-
-		final int INCREMENT = 1;
-		final int BOUNDS = bounds;
+		double red = (c.getRed());
+		double green = (c.getGreen());
+		double blue = (c.getBlue());
 
 		for(int i = index; i < pieces.length; i++){
-			int tempRed = pieces[i].getRed();
-			int tempGreen = pieces[i].getGreen();
-			int tempBlue = pieces[i].getBlue();
+			double tempRed = pieces[i].getRed();
+			double tempGreen = pieces[i].getGreen();
+			double tempBlue = pieces[i].getBlue();
 
-			if(Math.abs(tempRed - red) <= BOUNDS
-					&& Math.abs(tempGreen - green) <= BOUNDS
-					&& Math.abs(tempBlue - blue) <= BOUNDS){
+			if(Math.abs(tempRed - red) <= marginOfError
+					&& Math.abs(tempGreen - green) <= marginOfError
+					&& Math.abs(tempBlue - blue) <= marginOfError){
 				return i;
 			}
 
 		}
 		for(int j = index; j > 0; j--){
-			int tempRed = pieces[j].getRed();
-			int tempGreen = pieces[j].getGreen();
-			int tempBlue = pieces[j].getBlue();
+			double tempRed = pieces[j].getRed();
+			double tempGreen = pieces[j].getGreen();
+			double tempBlue = pieces[j].getBlue();
 
-			if(Math.abs(tempRed - red) <= BOUNDS
-					&& Math.abs(tempGreen - green) <= BOUNDS
-					&& Math.abs(tempBlue - blue) <= BOUNDS){
+			if(Math.abs(tempRed - red) <= marginOfError
+					&& Math.abs(tempGreen - green) <= marginOfError
+					&& Math.abs(tempBlue - blue) <= marginOfError){
 				return j;
 			}
 
 		}
-		return colorCheck(pieces,index, c, BOUNDS + INCREMENT);
+		return colorCheck(pieces,index, c, marginOfError + (marginOfError / 10)); //TODO: define the increment for the marginOfError
 	}
 
 	/*
-	 * merge sort using overwitten compareTo method int Piece 
+	 * merge sort using overwritten compareTo method int Piece
 	 */
 
 	public static void mergeSort(Piece[] list){
 		int length = list.length;
-		if(length < 2) return;
+		if(length < 2)
+			return;
 		int mid = length/2;
 		Piece[] leftArr = new Piece[mid];
 		Piece[] rightArr = new Piece[length - mid];
@@ -234,8 +222,8 @@ public class Mosaic extends Application {
 	}	
 	/* changes color values from a range of 0.0 - 1.0 into the range 0 - 255
 	 */
-	public static int toWebColor(double x){
-		return (int)(x * 255);
-	}
+	//public static int toWebColor(double x){
+		//return (int)(x * 255);
+	//}
 
 }
--- src/Piece.java
+++ src/Piece.java
@@ -15,29 +15,24 @@ import javafx.scene.paint.Color;
 public class Piece implements Comparable<Piece>{
 
 	private Color avgColor;
-	private Color greyScale;
-	//private int avgVal;
-	private String mainColor;
-	//private int width, height;
 	private double width, height;
-	//private BufferedImage image;
 	private Image image;
-	//private int red, green, blue, grey;
 	private double red, green, blue;
 	private double brightness;
 	
 	private final double opacity = 0;
-	
-	//public Piece(BufferedImage image){
+
 	public Piece(Image image){
 		this.image = image;
 		width = image.getWidth();
 		height = image.getHeight();
-		
+		double averageRed = 0;
+		double averageGreen = 0;
+		double averageBlue = 0;
+
 		Color[][] array = new Color[(int) width][(int) height];
 		for(int i = 0; i < width; i++){
 			for(int j = 0; j < height; j++){
-				//array[i][j] = new Color(image.getRGB(i,j));
 				PixelReader pr = image.getPixelReader();
 				array[i][j] = pr.getColor(i,j);
 			}
@@ -45,85 +40,54 @@ public class Piece implements Comparable<Piece>{
 		
 		for(Color[] s : array){
 			for(Color p : s){
-				red += p.getRed();
-				green += p.getGreen();
-				blue += p.getBlue();
+				averageRed += p.getRed();
+				averageGreen += p.getGreen();
+				averageBlue += p.getBlue();
 			}
 		}
-		red = red/(width*height);
-		green = green/(width*height);
-		blue = blue/(width*height);
-		//grey = ((int)(red * 0.21 + green * 0.72 + blue * 0.07));
-		//grey = (red * 0.21 + green * 0.72 + blue * 0.07);
-		avgColor = new Color(red, green, blue, opacity);
+
+		averageRed = averageRed/(width*height);
+		averageGreen = averageGreen/(width*height);
+		averageBlue = averageBlue/(width*height);
+		avgColor = new Color(averageRed, averageGreen, averageBlue, opacity);
+		red = averageRed;
+		green = averageGreen;
+		blue = averageBlue;
 		brightness = avgColor.getBrightness();
-		//avgVal = avgColor.getRGB();
-		//greyScale = new Color(grey, grey, grey, opacity);
+
 	}
 	
 	public Color getAvgColor(){
 		return avgColor;
 	}
-	/*public int avgBright(){
-		return (avgColor.getRed() + avgColor.getGreen() + avgColor.getBlue())/3; 
-	}*/
-	
-	/*public Color getGrey(){
-		return avgColor.grayscale();
-	}*/
-	public int getBrightness(){
-		return (int) (brightness * 255);
+
+	public double getBrightness(){
+		return (brightness);
 	}
-	public int getRed(){
-		return  (int) (red * 255);
+	public double getRed(){
+		return  (red);
 	}
-	public int getGreen(){
-		return  (int) (green * 255);
+	public double getGreen(){
+		return  (green);
 	}
-	public int getBlue(){
-		return (int) (blue * 255);
+	public double getBlue(){
+		return (blue);
 	}
 	public Image getImage(){
 		return image;
 	}
-	/*public Color getGreyScale(){
-		return greyScale;
-	}
-	public int getGrey(){
-		return grey;
-	}
-	public int getRed(){
-		return red;
-	}
-	public int getGreen(){
-		return green;
-	}
-	public int getBlue(){
-		return blue;
-	}
-	public BufferedImage getImage(){
-		return image;
-	}*/
-	
+
 	@Override
 	public int compareTo(Piece o){
-		int tempBr = o.getBrightness();
-		if((int)(brightness * 255) > tempBr){
+		double tempBr = o.getBrightness();
+		if(brightness > tempBr){
 			return 1;
 		}
-		if((int)(brightness * 255) < tempBr){
+		if(brightness < tempBr){
 			return -1;
 		}
 		return 0;
-		/*
-		if(this.avgBright() > o.avgBright()){
-			return 1;
-		}
-		if(this.avgBright() < o.avgBright()){
-			return -1;
-		}
-		return 0;
-		*/
+
 		
 	}
 }
