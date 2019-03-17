f85d874eb533a763fbca7ec77b37ceffbd59f3bd
Code-Conjuror <sadzune@gmail.com>
2019-03-17 13:00:10 -0700

    Started new structure of program: -Created TileList object to handle the initialization, sorting, and access of "Pieces" -Creating Tile object to replace the Piece object -MosaicBuilder now only implements methods from Mosaic and TileList

---
+++ src/back/Mosaic.java
@@ -0,0 +1,65 @@
+package back;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
+
+public class Mosaic {
+    private Image img;
+    private Image completedMosaic;
+
+
+    public void allocateTiles(int tileSizeX,int tileSizeY,TileList tileList) {
+
+        int numberOfTilesX = ((int) img.getWidth()) / tileSizeX;
+        int numberOfTilesY = ((int) img.getHeight()) / tileSizeY;
+
+        PixelReader pr = img.getPixelReader();
+        Thread[] tileBuilders = new TileBuilder[numberOfTilesX * numberOfTilesY];
+        int index;
+        for (int x = 0; x < numberOfTilesX; x++) {
+            for (int y = 0; y < numberOfTilesY; y++) {
+                index = (numberOfTilesY * x) + y;
+
+                tileBuilders[index] = new TileBuilder(
+                        x * tileSizeX, y * tileSizeY, tileSizeX, tileSizeY,
+                        pieces, mosaicArr, pr);
+
+                tileBuilders[index].start();
+            }
+        }
+        for(Thread t : tileBuilders) {
+            try {
+                t.join();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+
+
+        WritableImage writableMosaic = new WritableImage(imageWidth, imageHeight);
+        Image tempTile;
+        PixelReader pixelReader;
+        PixelWriter pixelWriter = writableMosaic.getPixelWriter();
+        for(int x = 0; x < numberOfTilesX; x++ ){
+            for(int y = 0; y < numberOfTilesY; y++){
+                tempTile = ImageTransformer.reduce(mosaicArr[x][y], tileSizeX, tileSizeY);
+                pixelReader = tempTile.getPixelReader();
+                pixelWriter.setPixels(x*tileSizeX, y*tileSizeY, tileSizeX, tileSizeY, pixelReader, 0, 0);
+
+            }
+        }
+
+        mosaic = (Image) writableMosaic;
+    }
+
+    public void setImage(Image img){
+        this.img = img;
+    }
+
+    public Image getCompletedMosaic() {
+        return completedMosaic;
+    }
+}
--- src/back/MosaicBuilder.java
+++ src/back/MosaicBuilder.java
@@ -6,13 +6,17 @@ import java.io.File;
 
 public class MosaicBuilder{
 
-    private Piece[] pieces;
+    //private Piece[] pieces;
+    private TileList tileList;
     private int tileSizeX = 10;
     private int tileSizeY = 10;
-    private Image mosaic;
+    private Mosaic mosaic;
 
     public MosaicBuilder(File tileDir) {
-
+        tileList = new TileList();
+        tileList.initalize(tileDir);
+        tileList.sort();
+        /*
         File[] dirList = tileDir.listFiles();
 
         pieces = new Piece[dirList.length];
@@ -22,73 +26,28 @@ public class MosaicBuilder{
             pieces[i] = new Piece(temp);
         }
 
-        TileSorter.sort(pieces);
+        TileSorter.sort(pieces);*/
     }
 
     public void setImage(Image image){
-        mosaic = image;
+        mosaic.setImage(image);
     }
     public Image run(){
-        //this.tileSizeX = tileSizeX;
-        //this.tileSizeY = tileSizeY;
-        //Image img = new Image(imageFile.toURI().toString());
-        int imageWidth = (int) mosaic.getWidth();
-        int imageHeight = (int) mosaic.getHeight();
-        Image[][] mosaicArr = new Image[imageWidth/tileSizeX][imageHeight/tileSizeY];
-        initializeMosaicArr(mosaic, mosaicArr, imageWidth, imageHeight);
-        return mosaic;
+        //int imageWidth = mosaic.getWidth();
+        //int imageHeight = mosaic.getHeight();
+        //Image[][] mosaicArr = new Image[imageWidth/tileSizeX][imageHeight/tileSizeY];
+        //initializeMosaicArr(mosaic, mosaicArr, imageWidth, imageHeight);
+        initializeMosaic();
+        return mosaic.getCompletedMosaic();
     }
 
 
-    private void initializeMosaicArr(Image img, Image[][] mosaicArr, int imageWidth, int imageHeight) {
-        PixelReader pr = img.getPixelReader();
-        int numberOfTilesX = imageWidth / tileSizeX;
-        int numberOfTilesY = imageHeight / tileSizeY;
+    //private void initializeMosaicArr(Image img, Image[][] mosaicArr, int imageWidth, int imageHeight) {
+    private void initializeMosaic() {
 
-        Thread[] tileBuilders = new TileBuilder[numberOfTilesX * numberOfTilesY];
+        mosaic.allocateTiles(tileSizeX, tileSizeY, tileList);
 
-        int index;
-        for (int x = 0; x < numberOfTilesX; x++) {
-            for (int y = 0; y < numberOfTilesY; y++) {
-                index = (numberOfTilesY * x) + y;
-
-                tileBuilders[index] = new TileBuilder(
-                        x * tileSizeX, y * tileSizeY, tileSizeX, tileSizeY,
-                        pieces, mosaicArr, pr);
-
-                tileBuilders[index].start();
-            }
-        }
-        for(Thread t : tileBuilders) {
-            try {
-                t.join();
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-        }
-
-        WritableImage writableMosaic = new WritableImage(imageWidth, imageHeight);
-        Image tempTile;
-        PixelReader pixelReader;
-        PixelWriter pixelWriter = writableMosaic.getPixelWriter();
-        for(int x = 0; x < numberOfTilesX; x++ ){
-            for(int y = 0; y < numberOfTilesY; y++){
-                tempTile = ImageTransformer.reduce(mosaicArr[x][y], tileSizeX, tileSizeY);
-                pixelReader = tempTile.getPixelReader();
-                pixelWriter.setPixels(x*tileSizeX, y*tileSizeY, tileSizeX, tileSizeY, pixelReader, 0, 0);
-
-				/*ImageView tile = new ImageView();
-				tile.setImage(imageList[x][y]);
-				tile.setFitHeight(tileSizeY);
-				tile.setFitWidth(tileSizeX);
-				gp.add(tile, x, y);*/
-            }
-        }
-
-        mosaic = (Image) writableMosaic;
     }
-
-    //public void setTiles(File tileDir){
 
     public void setTileSize(int tileSizeX, int tileSizeY){
         this.tileSizeX = tileSizeX;
